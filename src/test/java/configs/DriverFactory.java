5ec46019ec89a37a470ed6ec6bd22b2a87a9650d
Rethmi Thennakoon <reththennakoon.edu@gmail.com>
2026-03-17 23:51:04 +0530

    first commit

---
+++ src/test/java/configs/DriverFactory.java
@@ -0,0 +1,32 @@
+package configs;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class DriverFactory {
+
+    private static WebDriver driver;
+
+    public  static WebDriver initBrowser(String browser) {
+        if(browser.equalsIgnoreCase("chrome")) {
+            WebDriverManager.chromedriver().setup();
+            driver = new ChromeDriver();
+        }
+        else {
+            throw new RuntimeException("we do not support chromedriver");
+        }
+        return driver;
+
+    }
+    public static WebDriver getDriver() {
+        return driver;
+    }
+    public  static  void quitDriver() {
+        if(driver != null) {
+            driver.quit();
+            driver = null;
+        }
+    }
+}
---
+++ src/test/resources/features/Ecommerce.feature
@@ -0,0 +1,6 @@
+Feature:
+
+    Scenario:
+      Given I am on the landing page
+      Then User clicks on login button
+      Then User enters email and password
